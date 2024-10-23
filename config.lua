0b488440fcd57e5d83e43fa9385243adc950df5f
v-de-vinicius <vinicius.matosfelix@gmail.com>
2024-10-23 00:18:30 -0300

    feat: git plugins setup

--- config.lua
+++ config.lua
@@ -1,3 +1,4 @@
+require("remap")
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Example configs: https://github.com/LunarVim/starter.lvim
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
@@ -38,6 +39,37 @@ lvim.plugins = {
       require('solarized').setup(opts)
       vim.cmd.colorscheme 'solarized'
     end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = {"nvim-lua/plenary.nvim"},
+    config = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+      vim.keymap.set("n", "<leader>a", mark.add_file)
+      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+      vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+      vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+      vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+      vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+    end,
+  },
+  {
+    "ThePrimeagen/vim-be-good"
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>ph", ":Gitsigns preview_hunk<CR>", {})
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>ga", ":Git add -i", {})
+      vim.keymap.set("n", "<leader>gm", ":Git commit -m", {})
+    end,
   }
 }
 lvim.autocommands = {
@@ -50,4 +82,6 @@ lvim.autocommands = {
   }
 }
 lvim.transparent_window = true
-lvim.colorscheme = "solarized"
+lvim.colorscheme = "solarized_light"
+vim.opt.relativenumber = true
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
